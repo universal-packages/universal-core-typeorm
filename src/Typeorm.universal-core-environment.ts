35287e31ec5bbf76ed6ae5191cd045b754bace66
David De Anda <omarandstuff@gmail.com>
2024-07-16 11:17:59 -0600

    Environment tide to right task

--- src/Typeorm.universal-core-environment.ts
+++ src/Typeorm.universal-core-environment.ts
@@ -2,7 +2,7 @@ import { CoreEnvironment } from '@universal-packages/core'
 
 export default class TypeormEnvironment extends CoreEnvironment {
   public static readonly onlyFor = 'tasks'
-  public static readonly tideTo = 'typeorm-task'
+  public static readonly tideTo = 'typeorm'
 
   public beforeModulesLoad(): void {
     global['CORE_TYPE_ORM_TASK'] = true
